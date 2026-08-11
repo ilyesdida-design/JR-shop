3b6e67bb76f93558a2715281f52639ed6da303ec
ilyesdida-design <ilyesdida@hotmail.fr>
2026-08-11 23:52:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,22 @@
-const SUPABASE_URL = "https://cstjgsuehmqcolajspqh.supabase.co";
+```javascript
+/* =========================================================
+   JR SHOP - SUPABASE CONFIG
+========================================================= */
+
+const SUPABASE_URL =
+  "https://cstjgsuehmqcolajspqh.supabase.co";
 
 const SUPABASE_ANON_KEY =
   "sb_publishable_e_wQxqCrx21Qq1kRYKjFMg_yR6TQfKX";
 
-window.supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
+
+/* =========================================================
+   SUPABASE CLIENT
+========================================================= */
+
+const supabaseClient =
+  window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+  );
+```
