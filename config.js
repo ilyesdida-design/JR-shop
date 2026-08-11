b21f3ba889b0a36a44b66486ee2b51eddc488a0f
ilyesdida-design <ilyesdida@hotmail.fr>
2026-08-11 05:18:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,9 @@
 const SUPABASE_URL = "https://cstjgsuehmqcolajspqh.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_e_wQxqCrx21Qq1kRYKjFMg_yR6TQfKX";
+
+const SUPABASE_ANON_KEY =
+  "sb_publishable_e_wQxqCrx21Qq1kRYKjFMg_yR6TQfKX";
 
 const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
-
-const money = (n) =>
-  new Intl.NumberFormat("fr-DZ").format(Number(n || 0)) + " DA";
