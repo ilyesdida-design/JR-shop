3791334f38893d0708fe0d8b917e17bcb3901aaa
ilyesdida-design <ilyesdida@hotmail.fr>
2026-08-11 08:32:00 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,16 +3,7 @@ const SUPABASE_URL = "https://cstjgsuehmqcolajspqh.supabase.co";
 const SUPABASE_ANON_KEY =
   "sb_publishable_e_wQxqCrx21Qq1kRYKjFMg_yR6TQfKX";
 
-if (
-  typeof supabase === "undefined" ||
-  typeof supabase.createClient !== "function"
-) {
-  console.error("Supabase library is not loaded correctly.");
-} else {
-
-  const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-  );
-
-}
+window.supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON_KEY
+);
