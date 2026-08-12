102d4daa39dfe857c95aebf292331346fc3c07f9
ilyesdida-design <ilyesdida@hotmail.fr>
2026-08-12 02:46:56 +0200

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://cstjgsuehmqcolajspqh.supabase.co";
-
-const SUPABASE_ANON_KEY =
-  "sb_publishable_e_wQxqCrx21Qq1kRYKjFMg_yR6TQfKX";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
